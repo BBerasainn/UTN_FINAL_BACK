db3779608dce544093cef0f5a27c99a665731df3
BBerasainn <bb@bsmartlab.com>
2025-12-01 22:52:24 -0300

    change port email

--- src/utils/sendVerificationEmail.js
+++ src/utils/sendVerificationEmail.js
@@ -6,8 +6,8 @@ export async function sendVerificationEmail(user, token) {
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,      
-    port: Number(process.env.SMTP_PORT), 
-    secure: Number(process.env.SMTP_PORT) === 465, 
+    port: 465, 
+    secure: true, 
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
