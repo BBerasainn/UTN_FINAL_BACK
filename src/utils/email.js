e1bfad40c7e310a691889374e395027d98901614
BBerasainn <bb@bsmartlab.com>
2025-12-01 21:48:10 -0300

    fix gmail

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,16 +2,16 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Transporter GMAIL ✔
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,            
-  port: Number(process.env.SMTP_PORT),     
-  secure: true,                            
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),  
+  secure: false,                       
   auth: {
-    user: process.env.SMTP_USER,          
-    pass: process.env.SMTP_PASS            
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   },
 });
+
 
 export async function sendEmail(to, subject, html) {
   try {
