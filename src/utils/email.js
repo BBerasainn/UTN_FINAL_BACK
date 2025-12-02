33e55c2e5594eaf4dc5e3541e95806ed47c12905
BBerasainn <bb@bsmartlab.com>
2025-12-01 22:13:27 -0300

    continue...

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,9 +5,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-console.log("DEBUG SMTP_USER:", process.env.SMTP_USER);
-console.log("DEBUG SMTP_PASS:", process.env.SMTP_PASS ? "OK" : "MISSING");
-
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,     
   port: Number(process.env.SMTP_PORT), 
@@ -15,7 +12,7 @@ export const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  },
+  }
 });
 
 export async function sendEmail(to, subject, html) {
@@ -30,6 +27,8 @@ export async function sendEmail(to, subject, html) {
     console.log("📨 Email enviado a:", to);
   } catch (err) {
     console.error("❌ Error enviando email:", err);
+    console.log("DEBUG SMTP_USER:", process.env.SMTP_USER),
+    console.log("DEBUG SMTP_PASS:", process.env.SMTP_PASS ? "OK" : "MISSING");
   }
 }
 
